e59607a985862115e02079404c26cc6270c51e70
Ishan D PaPathirana <125178188+dinelka01@users.noreply.github.com>
2024-04-16 23:07:31 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCVu4tHdAqZ6ndMBacxu3jVOSjSnUgJ9FE",
+    authDomain: "automated-wheelchair-1bb63.firebaseapp.com",
+    projectId: "automated-wheelchair-1bb63",
+    storageBucket: "automated-wheelchair-1bb63.appspot.com",
+    messagingSenderId: "701784452867",
+    appId: "1:701784452867:web:2a84e60cab722173af8336"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+  
